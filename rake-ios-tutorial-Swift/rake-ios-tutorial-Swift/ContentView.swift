a5e8658593d80ff54011e55f28309befc2eb58e6
Hyeyoung Song <hyeyoung.song@sk.com>
2020-07-07 14:48:35 +0900

    [RAKE-630] SwiftUI 작성 완료

--- rake-ios-tutorial-Swift/rake-ios-tutorial-Swift/ContentView.swift
+++ rake-ios-tutorial-Swift/rake-ios-tutorial-Swift/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
         VStack(){
             Text("Rake iOS Swift Tutorial")
                 .font(.title)
+                .padding(30)
             
             Button(action: {
                 do {
@@ -30,15 +31,32 @@ struct ContentView: View {
             }) {
                 Text("TRACK")
                     .fontWeight(.heavy)
-            }
+                    .frame(width: 225.0, height: 84.0, alignment: .center)
+            }.buttonStyle(CustomButtonStyle())
+                .padding(30)
             
             Button(action: {
                 self.rake?.flush()
             }) {
                 Text("FLUSH")
                     .fontWeight(.heavy)
-            }
+                    .frame(width: 225.0, height: 84.0, alignment: .center)
+            }.buttonStyle(CustomButtonStyle())
+                .padding(30)
         }
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    var color: Color = .blue
+    
+    public func makeBody(configuration: CustomButtonStyle.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .background(RoundedRectangle(cornerRadius: 5).fill(color))
+            .compositingGroup()
+            .opacity(configuration.isPressed ? 0.5 : 1.0)
+            .scaleEffect(configuration.isPressed ? 0.8 : 1.0)
     }
 }
 
