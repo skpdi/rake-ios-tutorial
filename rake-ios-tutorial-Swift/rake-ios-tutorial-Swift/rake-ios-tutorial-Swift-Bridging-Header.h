5bc62a5562adfa8ad84c094a8b9bbb76c764ecbe
Hyeyoung Song <hyeyoung.song@sk.com>
2020-07-07 11:24:22 +0900

    [RAKE-630] Swift 튜토리얼에 Rake.framework 사용을 위해 Bridging Header 추가

---
+++ rake-ios-tutorial-Swift/rake-ios-tutorial-Swift/rake-ios-tutorial-Swift-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  rake-ios-tutorial-Swift-Bridging-Header.h
+//  rake-ios-tutorial-Swift
+//
+//  Created by 1000731 on 2020/07/07.
+//  Copyright © 2020 1000731. All rights reserved.
+//
+
+#ifndef rake_ios_tutorial_Swift_Bridging_Header_h
+#define rake_ios_tutorial_Swift_Bridging_Header_h
+
+
+#endif /* rake_ios_tutorial_Swift_Bridging_Header_h */
+
+#import <Rake/Rake.h>
