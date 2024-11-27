6862de495630ac1ccc71ab2c42c70f8220ec4929
Hyeyuong Song <hyeyoung.song@sk.com>
2024-11-27 11:15:58 +0900

    [RAKE-805] SwiftUI 예제 프로젝트 추가. 기존 프로젝트 재정비중

---
+++ rake-ios-tutorial-SwiftUI/rake-ios-tutorial-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rake-ios-tutorial-SwiftUI
+//
+//  Created by 송혜영(Hyeyoung Song)/Data Infrastructure팀/SKP on 11/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rake-ios-tutorial-SwiftUI/rake-ios-tutorial-SwiftUI/rake_ios_tutorial_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  rake_ios_tutorial_SwiftUIApp.swift
+//  rake-ios-tutorial-SwiftUI
+//
+//  Created by 송혜영(Hyeyoung Song)/Data Infrastructure팀/SKP on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct rake_ios_tutorial_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
